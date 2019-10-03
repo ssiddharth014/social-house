ea0ad7d77b6117118c9afe82135243e8c4886ad7
siddharth <ssiddharth014@gmail.com>
2019-10-04 02:43:46 +0530

    set up view engine: ejs

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const express=require('express');
 const app= express();
 const port=8000;
 
-
+ // middleware : use express router 
 app.use('/',require('./routes/index'));
+
+// set up of view engine
+
+app.set('view engine','ejs');
+app.set('views','./views');
 
 app.listen(port,function(err,){
 
